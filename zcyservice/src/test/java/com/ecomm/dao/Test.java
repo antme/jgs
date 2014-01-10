f9c51992b9ff8e5e3dfb572ea23b6ff56c2c9015
dylan <lieping.chen@yulor.com>
2014-01-10 14:51:18 +0800

    update page

--- zcyservice/src/test/java/com/ecomm/dao/Test.java
+++ zcyservice/src/test/java/com/ecomm/dao/Test.java
@@ -1,11 +1,16 @@
 package com.ecomm.dao;
 
+import com.zcy.util.PdfUtil;
+
 public class Test {
 
 	public static void main(String[] args) {
 
-		String s = "卷宗目录";
-		System.out.println(s.charAt(0));
+//		String s = "卷宗目录";
+//		System.out.println(s.charAt(0));
+		
+		
+		System.out.println(PdfUtil.getLines("/Users/ymzhou/Documents/TDSH-CG-2013-0258_s.pdf", 0, 1));
 
 	}
 
